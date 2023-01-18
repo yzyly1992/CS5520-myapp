c26a81e88c776abfa3d3997d738dec92ac53fc26
David Yang <zyang03@risd.edu>
2023-01-17 18:40:26 -0800

    add input component

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Header from "./components/Header";
 
 export default function App() {
+  const [text, setText] = useState();
   const name = "my awesome app";
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header name="awesome app" />
+      <Header appName="my awesome app" />
+      <TextInput style={{ backgroundColor: 'red', }} placeholder='please type here' onChangeText={setText} />
+      <Text>{text}</Text>
     </View>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Header(props) {
+export default function Header({ appName }) {
   return (
     <View>
-      <Text>Welcome to {props.name}</Text>
+      <Text>Welcome to {appName}</Text>
     </View>
   )
 }
