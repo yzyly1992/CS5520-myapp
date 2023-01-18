8bb490ba64522f1b77a3bea56e2b38ba14fd8853
David Yang <zyang03@risd.edu>
2023-01-17 18:06:40 -0800

    create header component and pass the name through props

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from "./components/Header";
 
 export default function App() {
+  const name = "my awesome app";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header name="awesome app" />
     </View>
   );
 }
