abdf5539c8464c13b55cff391d0c42edf435fae0
David Yang <zyang03@risd.edu>
2023-01-24 17:29:16 -0800

    adds confirm button to pass data and clean input

--- components/Input.js
+++ components/Input.js
@@ -1,14 +1,23 @@
-import { View, TextInput } from 'react-native'
+import { View, TextInput, Button } from 'react-native'
 import { useState } from 'react'
 
 export default function Input({ sendChangedText }) {
-const [text, setText] = useState();
+  const [text, setText] = useState();
+  function buttonPressed() {
+    sendChangedText(text);
+    setText();
+  }
+
   return (
     <View>
-      <TextInput style={{ backgroundColor: 'red', }} placeholder='please type here' onChangeText={(changedText) => {
+      <TextInput value={text} style={{ backgroundColor: 'red', }} placeholder='please type here' onChangeText={(changedText) => {
         setText(changedText);
-        sendChangedText(changedText);
+        style={backgroundColor: 'red',}
       }} />
+      <Button
+        title="Confirm"
+        onPress={buttonPressed}
+      />
     </View>
   )
 }
