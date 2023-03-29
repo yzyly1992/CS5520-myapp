d6b43be80b3ed5d34badca2d408b2a7914964981
David Yang <davidyang@Davids-MacBook-Pro.local>
2023-03-28 17:28:51 -0700

    add show image in goal

--- components/GoalDetail.js
+++ components/GoalDetail.js
@@ -1,9 +1,28 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { AntDesign } from '@expo/vector-icons'
 import GoalUsers from './GoalUsers';
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from '../Firebase/firebase-setup'
 
 export default function GoalDetail({ route, navigation }) {
+  const [url, setUrl] = useState("");
+  useEffect(()=>{
+    async function getImageUrl() {
+      try {
+      const reference = ref(storage, route.params.goalItem.imageURI);
+      const uri = await getDownloadURL(reference);
+      setUrl(uri)
+      console.log(uri);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getImageUrl();
+    // console.log(route.params.goalItem.imageURI);
+
+  },[])
+
   useEffect(()=>{
     navigation.setOptions({
       title: route.params.goalItem.text,
@@ -17,12 +36,13 @@ export default function GoalDetail({ route, navigation }) {
     return () => {
       console.log("unsubscribe");
     };
-  })
+  },[])
 
   return (
     <View>
       <Text>{route.params.goalItem.id}</Text>
       <Text>{route.params.goalItem.text}</Text>
+      {url && <Image source={{uri:url}} style={{width:100, height:100}}/>}
       <GoalUsers />
     </View>
   )
